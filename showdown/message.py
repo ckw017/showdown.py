7cf03ab169e1b5c35799705f61166fd3f25c74ad
Chris Wong <chriskw.xyz@gmail.com>
2019-01-13 09:29:22 -0800

    Added docstrings and __str__ methods

--- showdown/message.py
+++ showdown/message.py
@@ -1,46 +1,108 @@
+# -*- coding: utf-8 -*-
+"""Module for showdown's ChatMessage and PrivateMessage classes"""
 import time
 from . import user, utils
 
 class ChatMessage:
-    def __init__(self, room_id, inp_type, *params, client=None):
+    """
+    Class representing a chat message in a showdown chat room.
+
+    Args:
+        room_id (obj:`str`) : The ID of the room in which the message was sent
+        timestamp (obj:`int` or None) : The timestamp when the message was sent
+        author_str (obj:`str`) : A string representing the name and rank of the
+            author
+        content (obj:`str`) : A string representing the content of the message
+        client (obj:`showdown.client.Client`, optional) : The Client to be used
+            for the objects reply method
+
+        Attributes:
+            room_id (obj:`str`) : The ID of the room in which the message was sent
+            timestamp (obj:`int` or None) : The timestamp when the message was sent
+            author_str (obj:`str`) : A string representing the name and rank of the
+                author
+            content (obj:`str`) : A string representing the content of the message
+            client (obj:`showdown.client.Client` or None) : The Client to be used
+                for the objects reply method
+    """
+    def __init__(self, room_id, timestamp, author_str, content, client=None):
         self.room_id = room_id
-        if inp_type == 'c':
-            self.timestamp = None
-        elif inp_type == 'c:':
-            self.timestamp = int(params[0])
-            params = params[1:]
-        author_str = params[0]
-        self.client = client
+        self.timestamp = timestamp
         self.author = user.User(author_str, client=client)
-        self.content = '|'.join(params[1:])
+        self.content = content
+        self.client = client
 
     def __repr__(self):
         return '<ChatMessage ({}) [{}] {}: {}>'.format(
                  self.room_id, 
-                 self.timestamp, 
+                 utils.timestamp_to_hh_mm_ss(self.timestamp), 
                  str(self.author),
-                 abbreviate(self.content)
+                 utils.abbreviate(self.content)
                )
+
+    def __str__(self):
+        return '({}) [{}] {}: {}'.format(
+                self.room_id,
+                utils.timestamp_to_hh_mm_ss(self.timestamp), 
+                str(self.author),
+                self.content
+            )
 
     @utils.require_client
     async def reply(self, content, client=None):
+        """
+        Uses the provided client or the object's client attribute to send a message
+        to the message's room of origin.
+
+        Args:
+            room_id (obj:`str`) : The content of the reply
+            client (obj:`showdown.client.Client`, optional) : The client used to 
+                reply to the message. This will default to the message's client
+                object
+        """
         await client.say(self.room_id, content)
 
 class PrivateMessage:
-    def __init__(self, author_str, recipient_str, *content, client=None):
+    """
+    Class representing a private message from another user.
+
+    Args:
+        author_str (obj:`str`) : A string representing the name and rank of the
+            author
+        recipient_str (obj:`str`) : A string representing the name and rank of the
+            recipient
+    """
+    def __init__(self, author_str, recipient_str, content, client=None):
         self.timestamp = int(time.time())
         self.author = user.User(author_str, client=client)
         self.recipient = user.User(recipient_str, client=client)
-        self.content = '|'.join(content)
+        self.content = content
         self.client = client
 
     def __repr__(self):
         return '<PrivateMessage ({}->{}) [{}]: {}>'.format(
                str(self.author), 
                str(self.recipient),
-               self.timestamp, 
+               utils.timestamp_to_hh_mm_ss(self.timestamp), 
                utils.abbreviate(self.content))
+
+    def __str__(self):
+        return '(private message) [{}] {}: {}'.format(
+                utils.timestamp_to_hh_mm_ss(self.timestamp), 
+                str(self.author),
+                self.content
+            )
 
     @utils.require_client
     async def reply(self, content, client=None):
+        """
+        Uses the provided client or the object's client attribute to send a message
+        to the message's room of origin.
+
+        Args:
+            room_id (obj:`str`) : The content of the reply
+            client (obj:`showdown.client.Client`, optional) : The client used to 
+                reply to the message. This will default to the message's client
+                object
+        """
         await client.private_message(self.author.id, content)
