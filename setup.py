f344c693f551930288e81c012ee29cf600117650
Chris Wong <chriskw.xyz@gmail.com>
2019-01-13 22:16:51 -0800

    Incremented version number

--- setup.py
+++ setup.py
@@ -6,19 +6,36 @@ with open('README.md', 'r') as readme:
 with open('requirements.txt', 'r') as reqf:
     requirements = reqf.read().splitlines()
 
+version = ''
+with open('discord/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('version is not set')
+
 setuptools.setup(
-    name='showdownpy',
-    version='0.0.3',
+    name='showdown.py',
+    version=version,
     author='Chris K. W.',
     author_email='chriskw.xyz@gmail.com',
+    license='MIT'
+    include_package_data=True,
     description='An extendable client for interacting with Pokemon Showdown',
     long_description=long_desc,
     long_description_content_type='text/markdown',
-    url='',
+    url='https://github.com/ckw017/showdown.py',
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python 3.5+",
-        "Operating System :: OS Independent",
-    ],
     install_requires=requirements,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Topic :: Internet',
+        'Topic :: Utilities',
+        'Topic :: Games/Entertainment :: Turn Based Strategy'.
+        'Topic :: Communications :: Chat'
+    ]
 )
--- showdown/__init__.py
+++ showdown/__init__.py
@@ -3,7 +3,11 @@
 __title__   = 'showdown'
 __author__  = 'chriskw'
 __license__ = 'MIT'
-__version__ = '0.0.4'
+__version__ = '0.1.0'
 
 from .client import Client
 from .user import User
+from .server import Server
+from .message import ChatMessage, PrivateMessage
+from .room import Room, Battle
+from . import utils
